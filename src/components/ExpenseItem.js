11b942a08deab098e9b324ea16e4890d225b2289
Benasi000 <bendzo13@gmail.com>
2022-07-10 18:55:32 +0200

    MoneyTracker: [0006] - Created a new css file where we input our css code to style ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
     return (
-        <div>
+        <div className='expense-item'>
             <div>Date</div>
-            <div>
+            <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
-                <div>60$</div>
+                <div className='expense-item__price'>60.00$</div>
             </div>
         </div>
     );
