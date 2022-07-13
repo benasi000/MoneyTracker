457aaef7bc44e5e95f1417bd5a228f9b37d283a6
Benasi000 <bendzo13@gmail.com>
2022-07-13 21:47:07 +0200

    MoneyTracker: [0014] - updating the functions syntax's on all ".js" files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,11 +2,11 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate 
+            <ExpenseDate
                 date={props.date}
             />
             <div className='expense-item__description'>
